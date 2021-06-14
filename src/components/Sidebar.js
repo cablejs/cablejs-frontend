cd0d7af66c9e17c8dd9ab9a0f035756ade1bbf80
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-06-13 22:28:00 -0600

    Add initial files

---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import "./Sidebar.css";
+
+export default function Sidebar() {
+    return (
+        <div className="sideBar-As1F2d">
+            <div className="guild-9a7A4c" />
+        </div>
+    );
+}
