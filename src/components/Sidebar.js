cd3103ea6880a4a312506d3bf739689dda54e4d5
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-06-15 12:03:26 -0600

    Add nav holder

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,9 @@ import "./Sidebar.css";
 export default function Sidebar() {
     return (
         <div className="sideBar-As1F2d">
-            <div className="guild-9a7A4c" />
+            <nav className="guilds-8f20ea">
+                <div className="guild-9a7A4c" />
+            </nav>
         </div>
     );
 }
