7508c5ec0ebaad95d712e1fb21b2774b6b32db9c
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-06-15 17:32:46 -0600

    Add home redirect

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 import AppRenderer from "./AppRenderer";
 import Login from "./Login";
 
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export default function App() {
   return (
     <main>
       <Switch>
+        <Route path="/">
+          <Redirect to="/login" />
+        </Route>
         <Route path="/channels/:gid/:id">
           <AppRenderer />
         </Route>
